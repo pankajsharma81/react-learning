3a69f8bfbd40fe487e4cd053a98e37f9e4e5ac64
pankajsharma81 <pankajsharma88279@gmail.com>
2025-04-18 15:27:56 +0530

    react native method - React.createElement()

--- CAC/01vitereact/src/main.jsx
+++ CAC/01vitereact/src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
 import App from './App.jsx'
+import React from 'react'
+
+
+//* Not work here
+// const reactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "click me to visit google",
+// };
+
+//* this works here
+const reactElement = React.createElement(
+  'a',
+  {href:'https://google.com', target:'_blank'},
+  'click me to visit google'
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  reactElement
 )
