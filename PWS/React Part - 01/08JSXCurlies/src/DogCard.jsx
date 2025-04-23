c56ac60b79ca40316b3e1e6786fef6b4915842b1
pankajsharma81 <pankajsharma88279@gmail.com>
2025-04-24 02:10:59 +0530

    style in reactJs (JSX Curlies)

--- PWS/React Part - 01/08JSXCurlies/src/DogCard.jsx
+++ PWS/React Part - 01/08JSXCurlies/src/DogCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const DogCard = ({ name, image }) => {
   return (
     <>
-      <h1>{name}</h1>
+      <h1 style={{fontSize:"10px",color:"blue"}}>{name}</h1>
       <img src={image} />
     </>
   );
