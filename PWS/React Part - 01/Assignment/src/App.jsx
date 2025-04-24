c3b1f949bf29145ae0dcb4be1e3c050327bfb007
pankajsharma81 <pankajsharma88279@gmail.com>
2025-04-24 11:49:02 +0530

    assignment - Ques2

--- PWS/React Part - 01/Assignment/src/App.jsx
+++ PWS/React Part - 01/Assignment/src/App.jsx
@@ -1,12 +1,14 @@
-import Person from './Person'
-import './App.css'
+// import Person from './Person'
+import Button from "./Button";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <Person name="Pankaj" age="21" />
+      {/* <Person name="Pankaj" age="21" /> */}
+      <Button text="Submit" onClick={() => {console.log("Button Clicked!!")}}/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
