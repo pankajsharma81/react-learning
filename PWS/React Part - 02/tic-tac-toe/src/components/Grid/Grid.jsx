782b2ec6df6225934e8aa3925655be389448390c
pankajsharma81 <pankajsharma88279@gmail.com>
2025-04-29 15:12:39 +0530

    tic-tac-toe 2

--- PWS/React Part - 02/tic-tac-toe/src/components/Grid/Grid.jsx
+++ PWS/React Part - 02/tic-tac-toe/src/components/Grid/Grid.jsx
@@ -1,15 +1,60 @@
 import Card from "../Card/card";
 import "./Grid.css";
+import isWinner from "../helpers/checkWinner";
 
 import { useState } from "react";
 
 const Grid = ({ numberOfCards }) => {
-  const [board, setBoard] = useState(Array(numberOfCards).fill(""));
+  const [board, setBoard] = useState(Array(numberOfCards).fill("")); //no. of boards
+  const [turn, setTurn] = useState(true); // true --> O, false --> X
+  const [winner, setWinner] = useState(null);
+
+  function play(index) {
+    if (turn == true) {
+      board[index] = "O";
+    } else {
+      board[index] = "X";
+    }
+
+    const win = isWinner(board, turn ? "O" : "X");
+
+    if (win) {
+      setWinner(win);
+    }
+
+    setBoard([...board]);
+    setTurn(!turn);
+  }
+
+  function reset() {
+    setTurn(true);
+    setWinner(null);
+    setBoard(Array(numberOfCards).fill(""));
+  }
+
   return (
-    <div className="grid">
-      {board.map((el, idx) => (
-        <Card key={idx} />
-      ))}
+    <div className="grid-wrapper">
+      {winner && (
+        <>
+          <h1 className="turn-highlight">Winner is {winner} </h1>
+          <button className="reset" onClick={reset}>
+            Reset Game
+          </button>
+        </>
+      )}
+
+      <h1 className="turn-highlight">Current turn: {turn ? "O" : "X"} </h1>
+      <div className="grid">
+        {board.map((el, idx) => (
+          <Card
+            key={idx}
+            gameEnd={winner ? true : false}
+            onPlay={play}
+            player={el}
+            index={idx}
+          />
+        ))}
+      </div>
     </div>
   );
 };
