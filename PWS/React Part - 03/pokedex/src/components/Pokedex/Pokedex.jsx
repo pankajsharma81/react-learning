a4846682cd948d32ef03712d500a609abcd66432
pankajsharma81 <pankajsharma88279@gmail.com>
2025-05-07 07:38:35 +0530

    updated pokedex and add css

--- PWS/React Part - 03/pokedex/src/components/Pokedex/Pokedex.jsx
+++ PWS/React Part - 03/pokedex/src/components/Pokedex/Pokedex.jsx
@@ -1,13 +1,12 @@
-import React from "react";
+import './Pokedex.css'
 import Search from "../Search/Search";
 import PokemonList from "../PokemonList/PokemonList";
 
 const Pokedex = () => {
   return (
     <div className="pokedex-wrapper">
-      Pokedex
       <Search />
-      <PokemonList/>
+      <PokemonList />
     </div>
   );
 };
