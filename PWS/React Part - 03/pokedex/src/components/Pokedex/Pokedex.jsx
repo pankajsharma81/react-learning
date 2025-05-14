83559548eb0215af205380e4b75e2b2953c63acb
pankajsharma81 <pankajsharma88279@gmail.com>
2025-05-14 09:15:04 +0530

    add debouncing

--- PWS/React Part - 03/pokedex/src/components/Pokedex/Pokedex.jsx
+++ PWS/React Part - 03/pokedex/src/components/Pokedex/Pokedex.jsx
@@ -1,12 +1,17 @@
-import './Pokedex.css'
+import "./Pokedex.css";
+import { useState } from "react";
+
 import Search from "../Search/Search";
 import PokemonList from "../PokemonList/PokemonList";
+import PokemonDetails from "../PokemonDetails/PokemonDetails";
 
 const Pokedex = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  
   return (
     <div className="pokedex-wrapper">
-      <Search />
-      <PokemonList />
+      <Search updateSearchTerm={setSearchTerm} />
+      {(!searchTerm) ? <PokemonList /> : <PokemonDetails key={searchTerm} pokemonName={searchTerm} />}
     </div>
   );
 };
