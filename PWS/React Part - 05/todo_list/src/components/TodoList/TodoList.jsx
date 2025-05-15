5e10e8a11e6a50e88c02bfa2e9643af08a1ffece
pankajsharma81 <pankajsharma88279@gmail.com>
2025-05-15 22:17:49 +0530

    todo app

---
+++ PWS/React Part - 05/todo_list/src/components/TodoList/TodoList.jsx
@@ -0,0 +1,29 @@
+import Todo from "../Todo/Todo";
+
+const TodoList = ({ list, updateList }) => {
+  return (
+    <div>
+      {list.length > 0 &&
+        list.map((todo) => (
+          <Todo
+            key={todo.id}
+            id={todo.id}
+            isFinished={todo.finished}
+            todoData={todo.todoData}
+            // not get ......................................
+            changeFinished={(isFinished) => {
+              const updatedList = list.map((t) => {
+                if (t.id == todo.id) {
+                  todo.finished = isFinished;
+                }
+                return t;
+              });
+              updateList(updatedList);
+            }}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default TodoList;
